54bc4b78eba6c7e89fa1969f732221eec2c4da8c
Kahiro-M <k_matsu@riso.co.jp>
2019-12-29 14:14:56 +0900

    inputを複数行に変更

--- main.js
+++ main.js
@@ -4,5 +4,23 @@ function makeOutTxt(){
     const inTxt3 = document.getElementById("inTxt3");
     var outTxt = document.getElementById("outTxt");
 
-    outTxt.value = inTxt1.value+" "+inTxt2.value+" "+inTxt3.value;
+    outTxt.value = "";
+    const aryInTxt2 = inTxt2.value.split(/\r\n|\r|\n/);
+    const aryInTxt3 = inTxt3.value.split(/\r\n|\r|\n/);
+    if(aryInTxt2.length<aryInTxt3.length){
+        for (let i = 0; i <= aryInTxt3.length-aryInTxt2.length; i++) {
+            aryInTxt2.push("");
+        }
+    }
+    else if(aryInTxt3.length<aryInTxt2.length){
+        for (let i = 0; i <= aryInTxt2.length-aryInTxt3.length; i++) {
+            aryInTxt3.push("");
+        }
+    }
+    else {
+    }
+
+    for(var i=0;i<Math.max(aryInTxt2.length,aryInTxt3.length);i++){
+        outTxt.value += inTxt1.value+" "+aryInTxt2[i]+" "+aryInTxt3[i]+"\n";
+    }
 }
